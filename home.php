082c0bb425672bb22bf773e206b4f6ef4b4fe550
alexdesfinov <alexdesfinov@gmail.com>
2024-06-09 22:34:27 +0700

    first commit

---
+++ home.php
@@ -0,0 +1,21 @@
+<div class="page-header">
+    <h2>Penentuan Penilaian Kinerja Karyawan Metode AHP</h2>
+    Sistem ini membantu pengambil keputusan dalam menyeleksi karyawan kontrak BPJN Sumbar dengan membandingkan setiap karyawan kontrak pada setiap kriteria,
+    metode yang digunakan adalah AHP (<i>Analytical Hierarchy Process</i>).<br /><br />
+
+    <b>Proses yang Anda harus lakukan :</b>
+    <ol>
+        <li>Masukkan Kriteria yang dibutuhkan.</li>
+        <li>Masukkan Sub Kriteria dari setiap kriteria yang dibutuhkan.</li>
+        <li>Masukkan Alternatif/karyawan kontrak.</li>
+        <li>Mulai seleksi dengan metode AHP :
+            <ul>
+                <li>Masuk ke Nilai Bobot Kriteria dan isikan nilai perbandingan kriteria untuk menentukan bobot kriteria.</li>
+                <li>Masuk ke Nilai Bobot SubKriteria dan isikan nilai perbandingannya.</li>
+                <li>Masuk ke Nilai Bobot Alternatif dan isikan bobot alternatifnya.</li>
+                <li>Masuk ke Perhitungan untuk melihat hasil perhitungan dan perangkingan.</li>
+            </ul>
+        </li>
+        <li>Anda juga dapat mencetak data Kriteria, Sub Kriteria, Alternatif dan Rangking.</li>
+    </ol>
+</div>
